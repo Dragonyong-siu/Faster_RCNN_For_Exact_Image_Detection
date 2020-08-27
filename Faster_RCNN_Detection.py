88772c01f038e75c8d8996961137ea2843b1b72c
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-08-28 02:19:04 +0900

    Update Faster_RCNN_Detection.py

--- Faster_RCNN_Detection.py
+++ Faster_RCNN_Detection.py
@@ -1,8 +1,8 @@
 def Resize(roi, ratio_value):
-  xmax = int(roi[0] * ratio_value)
-  xmin = int(roi[1] * ratio_value)
-  ymax = int(roi[2] * ratio_value)
-  ymin = int(roi[3] * ratio_value)
+  xmax = (roi[0] * ratio_value)
+  xmin = (roi[1] * ratio_value)
+  ymax = (roi[2] * ratio_value)
+  ymin = (roi[3] * ratio_value)
   return (xmax, xmin, ymax, ymin)
 
 def Make_Forms(Tuple):
@@ -34,23 +34,24 @@ class Faster_RCNN_Detection(torch.utils.data.Dataset):
   
   def __getitem__(self, index):
     Dictionary = {}
+    Book = Faster_RCNN_Dataset(self.data)[index]
     
-    Full_Image_Array = Faster_RCNN_Dataset(self.data)[index]['Full_Image_Array']
-    Model_ids = Faster_RCNN_Dataset(self.data)[index]['Model_ids']
-    Guide = Faster_RCNN_Dataset(self.data)[index]['Guide_label']
-    Roi_box = Faster_RCNN_Dataset(self.data)[index]['Roi_box']
-    Roi_ratio = Faster_RCNN_Dataset(self.data)[index]['Roi_ratio']
+    Full_Image_Array = Book['Full_Image_Array']
+    Model_ids = Book['Model_ids']
+    Guide = Book['Guide_label']
+    Roi_box = Book['Roi_box']
+    Roi_ratio = Book['Roi_ratio']
 
     # Trained_Faster_RCNN : Model_output
     Reg_logits = Trained_Faster_RCNN(Model_ids)[1]
     Reg_logits = Reg_logits.cpu().detach()
-
-    # True_value
+    
+    # True_roi : (xmax, xmin, ymax, ymin)
     True_roi = []
     for i in range(len(Roi_box)):
       if Roi_box[i][0] == 1:
         True_roi.append((Roi_box[i][1], Reg_logits[i]))
-
+    
     # Trained_box
     Trained_box = []
     for j in range(len(True_roi)):
@@ -58,24 +59,27 @@ class Faster_RCNN_Detection(torch.utils.data.Dataset):
       Logit = True_roi[j][1]
       D_u = Make_Logits(T, Logit)
       Trained_box.append(D_u)
-
     # Ratio_value
     # Resized_box
     Ratio_value = Roi_ratio[0] / Roi_ratio[2]
     Resized_box = []
     for k in range(len(Trained_box)):
       D_roi = Trained_box[k]
-      Resized_roi = Resize(D_roi, Ratio_value)
+      D_x, D_y, D_w, D_h = Resize(D_roi, Ratio_value)
+      Resized_roi = (D_x + 0.5 * D_w,
+                     D_x - 0.5 * D_w,
+                     D_y + 0.5 * D_h,
+                     D_y - 0.5 * D_h)
       Resized_box.append(Resized_roi)
-
+    
     Copy_image = Full_Image_Array.copy()
-    Green_line = (125, 255, 51)
+    Green_box = (125, 255, 51)
     for n in range(len(Resized_box)):
       xmax, xmin, ymax, ymin = Resized_box[n]
       Copy_image = cv2.rectangle(Copy_image,
-                                (xmin, ymax),
-                                (xmax, ymin),
-                                color = Green_line,
+                                (int(xmin), int(ymax)),
+                                (int(xmax), int(ymin)),
+                                color = Green_box,
                                 thickness = 2)
     
     Detection_Image = Copy_image
