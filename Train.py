46e74934fa0b2a16ae45018e46248f5e0ffc099c
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-08-27 17:23:47 +0900

    Update Train.py

--- Train.py
+++ Train.py
@@ -56,11 +56,11 @@ def Train_Epoch(dataloader, model, optimizer, device):
                             guide_label)
     CLS_Loss, REG_Loss = Loss[0], Loss[1]
     CLS_Loss.backward(retain_graph = True)
-    #REG_Loss.backward()#retain_graph = True)
+    REG_Loss.backward()#retain_graph = True)
     optimizer.step()
     #optimizer.zero_grad()
     total_CLS_loss += CLS_Loss.item()
-    #total_REG_loss += REG_Loss.item()
+    total_REG_loss += REG_Loss.item()
     
     #del model_ids
     #del cls_label
@@ -69,9 +69,9 @@ def Train_Epoch(dataloader, model, optimizer, device):
     #del Logits
   
   Average_CLS_Loss = total_CLS_loss / len(dataloader)
-  #Average_REG_Loss = total_REG_loss / len(dataloader)
+  Average_REG_Loss = total_REG_loss / len(dataloader)
   print(" Average CLS Loss: {0:.2f}".format(Average_CLS_Loss))
-  #print(" Average REG Loss: {0:.2f}".format(Average_REG_Loss))
+  print(" Average REG Loss: {0:.2f}".format(Average_REG_Loss))
 
 def FIT(model, Epochs, Learning_Rate):
   #optimizer = torch.optim.SGD(model.parameters(),
