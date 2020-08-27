70ae44ab505fb085a623ea6c957c73a3106a6ab2
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-08-27 14:49:50 +0900

    Create  Adjust the number of data.py

---
+++ Adjust the number of data.py
@@ -0,0 +1,19 @@
+train_data_0 = []
+for i in range(0, 5716, 2):
+  train_data_0.append(Train_data[i])
+
+train_data_1 = []
+for i in range(0, 5715, 5):
+  train_data_1.append(Train_data[i])
+
+train_data_2 = []
+for i in range(0, 5500, 55):
+  train_data_2.append(Train_data[i])
+
+train_data_3 = []
+for i in range(0, 4500, 90):
+  train_data_3.append(Train_data[i])
+
+train_data_4 = []
+for i in range(0, 100, 5):
+  train_data_4.append(Train_data[i])
