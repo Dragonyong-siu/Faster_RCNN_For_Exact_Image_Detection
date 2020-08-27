e967d45308acd490cfca1537416c2b1ad45190cb
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-08-27 14:47:52 +0900

    Create Data_Load.py

---
+++ Data_Load.py
@@ -0,0 +1,8 @@
+import torchvision
+Train_data = torchvision.datasets.VOCDetection(root = './data',
+                                               year = '2012',
+                                               image_set = 'train',
+                                               download = True,
+                                               transform = None,
+                                               target_transform = None,
+                                               transforms = None)
